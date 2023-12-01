2643e31255c33c0cbf5c9272ce4603065fff31b2
IEPrasad <126505521+IEPrasad@users.noreply.github.com>
2023-12-01 22:50:43 +0530

    Add files via upload

---
+++ lesson6.py
@@ -0,0 +1,41 @@
+
+
+        # --------------Python GUI - 06 | Radio Buttons |-------------
+
+  # ** Checkboxes and Radio buttons are both types of GUI elements used for user input in 
+  #   graphical user interfaces, but they have different charateristics and use cases:
+
+  #   1: Chekbox:
+
+  #         ** A checkbox is a GUI element that allows users to select or deselect and  
+  #             option independently.
+          
+  #         ** You can have multiple checkboxes in a group, and users can choose any 
+  #             combination of options.
+          
+  #         ** Each checkbox is typically independent of others in the group.
+          
+  #         ** Checkboxes are suitable when users can choose multiple options simultaneously.
+
+
+  #   2: Radio Buttons:
+
+  #         ** A radion button, or option button, is a GUI element that presents a set of
+  #         mutually exclusive options.
+
+  #         ** Users can choose only one option from the group of radio buttons.
+
+  #         ** When one radion button in a group is selected, it automatically deselecteds
+  #             any other radio button in the same group.
+
+  #         ** Radio buttons are suitable when users need to make a single selection
+  #             from a set of options.
+
+  # In summary, checkboxes are used when users can make multiple selections from a group 
+  # of options, while radio buttons are used when users need to make a single, exclusive   
+  # selection from a set of options. The choice between checkboxes and radio buttons depends
+  # on the specific requirements of the user interface and the type of input you want to 
+  # collect from the user.
+
+
+
