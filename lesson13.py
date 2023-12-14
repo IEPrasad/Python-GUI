335be701c5a6d5acdcf7eedaae2b2d5306f87816
IEPrasad <126505521+IEPrasad@users.noreply.github.com>
2023-12-14 23:33:33 +0530

    Update lesson13.py

--- lesson13.py
+++ lesson13.py
@@ -16,7 +16,7 @@
 # root = tk.Tk()
 # root.title('Frames')
 # root.geometry('500x400')
-
+ 
 # frame1 = ttk.Frame(root, width=200, height=100, relief=tk.GROOVE)
 # # frame1.pack()
 # ## we can change the alignment like these, 
@@ -30,6 +30,8 @@
 # ----------- now let's see how to add a entry field 
 # for these frame -----------
 
+
+'''
 import tkinter as tk
 from tkinter import ttk
 
@@ -43,7 +45,7 @@ frame2.pack(side='left')
 # kalin api laba dunne master eka sadaha
 # root we. namuth api dan laba denne adala frame eke nama we
 entry = ttk.Entry(frame2) 
-frame2.propagate(False)
+frame2.pack_propagate(False)
 entry.pack(pady=10)
 
 ## but now when we add a entry or another thing we have lost over
@@ -53,15 +55,42 @@ entry.pack(pady=10)
 # add a padding we can use pady = 10 
 # or padx = 20
 
+root.mainloop()
 
+'''
+
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+root.title('Frames')
+root.geometry('500x400')
+
+frame3 = ttk.Frame(root, width=200, height=100, relief=tk.GROOVE)
+frame3.pack_propagate(False)
+frame3.pack(side='left')
+
+entry = ttk.Entry(frame3)
+entry.pack(pady=10)
+
+button = ttk.Button(frame3, text='Submit')
+button.pack()
+
+frame4 = ttk.Frame(root, width=200, height=100, relief=tk.GROOVE)
+frame4.pack_propagate(False)
+frame4.pack(pady=30)
+
+label = ttk.Label(frame4, text='Welcome')
+label.pack(pady=10)
+
+button2 = ttk.Button(frame4, text='Click Me')
+button2.pack(pady=10)
 
 
 root.mainloop()
 
 
-
-
-
+# ------------- 13 is over ------------------ 
 
 
 
